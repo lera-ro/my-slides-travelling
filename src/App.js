e5805e81a5b3558c74259b57d33c1867b13ad074
lera-ro <lera.rodoman@yandex.ru>
2024-08-25 22:03:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [travel, setTravel] = useState(0);
+  const {id, country, place, description, image} = data[travel];
+
+  const previousTravel = () => {
+    setTravel(travel => {
+      travel--;
+      if (travel < 0) {
+        return data.length - 1;
+      }
+      return travel;
+    })
+  }
+
+  const nextTravel = () => {
+    setTravel(travel => {
+      travel++;
+      if (travel > data.length - 1) {
+        travel = 0;
+      }
+      return travel;
+    })
+  }
+
+  return (<div> 
+
+    <div className='container'>
+     <button onClick={previousTravel}>Previous</button>
+      <h2>{country}</h2>
+     <button onClick={nextTravel}>Next</button>  
+    </div> 
+
+    <div className='section'>
+      <div className='box one'>
+        <img src={image} width="300px" alt='travel'/>
+      </div>
+        <div className='box two'>
+          <h1>{id} - {place}</h1>
+          <p>{description}</p>
+        </div>
     </div>
-  );
-}
+
+  </div>)}
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data = [
+    {
+        id: 1,
+        country: "Spain",
+        place: "Barcelona",
+        description: "Barcelona is a city with a wide range of original leisure options that encourage you to visit time and time again. Overlooking the Mediterranean Sea, and famous for Gaudí and other Art Nouveau architecture, Barcelona is one of Europe’s trendiest cities.",
+        image: "https://images.unsplash.com/photo-1608753527764-81065db0f78b?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 2,
+        country: "Thailand",
+        place: "Phuket",
+        description: "Phuket province is located in southern Thailand. It is the biggest Island of Thailand and sits on the Andaman sea. The nearest province to the north is Phang-nga and the nearest provinces to the east are Phang-nga and Krabi. Being a big Island, Phuket is surrounded by many magnificent Beaches such as Rawai, Patong, Karon, Kamala, Kata Yai, Kata Noi, and Mai Khao. Laem Phromthep viewpoint is said to feature the most beautiful sunsets in Thailand.",
+        image: "https://images.unsplash.com/photo-1655715707586-5448d68aae20?q=80&w=2018&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 3,
+        country: "Italy",
+        place: "Venice",
+        description: "Venice is built on 118 small islands that are separated by 150 canals. People cross the canals by many small bridges. They can also navigate across the city on boats, both rowing boats and motor boats. The most famous Venetian type of boat is called a gondola.",
+        image: "https://images.unsplash.com/photo-1480548004877-593316be2bd5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 4,
+        country: "France",
+        place: "Paris",
+        description: "Climb to the top of the Eiffel Tower, stroll down the Champs Elysées, visit the Louvre, see many shows and exhibitions, or simply wander along the banks of the Seine...read in French in the Tuileries garden, and quite simply take the time to experience the Parisian way of life!",
+        image: "https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?q=80&w=1600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {
+        id: 5,
+        country: "Turkey",
+        place: "Istanbul",
+        description: "The old walled city of Istanbul stands on a triangular peninsula between Europe and Asia. Sometimes as a bridge, sometimes as a barrier, Istanbul for more than 2,500 years has stood between conflicting surges of religion, culture, and imperial power. For most of those years it was one of the most coveted cities in the world.",
+        image: "https://images.unsplash.com/photo-1580327165924-af17f5ac47c4?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
